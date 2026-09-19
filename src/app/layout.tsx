00c2e3741a2b2edd9f317142a245c55dfb7d340a
aletorresprado <aletorresprado@live.com>
2026-09-19 00:22:05 -0300

    feat: add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AtlasChat from "@/components/Atlas/AtlasChat";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -76,6 +77,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full flex flex-col">
         {children}
         <AtlasChat />
+        <GoogleAnalytics gaId="G-03LJ5PFFM6" />
       </body>
     </html>
   );
